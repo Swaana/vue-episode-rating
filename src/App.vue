cab6eef8d70e783b271e85a513c9f1cbbe829044
Swaana Snorradottir <swaanas@gmail.com>
2025-06-18 09:08:01 +0200

    Add files to source control

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import { RouterView } from 'vue-router'
+import TheHeader from './components/TheHeader.vue'
+</script>
+
+<template>
+  <TheHeader />
+  <main class="page">
+    <RouterView />
+  </main>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,51 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="filters">
+    <div class="season-selection card">
+      <span>Season:</span>
+      <ul>
+        <li>
+          <span>1</span>
+          <input type="checkbox" name="season" value="1" />
+        </li>
+      </ul>
+    </div>
+  </div>
+  <div class="episode-list">
+    <div class="episode-container">
+      <div class="episode-info">
+        <div class="card">
+          <div>
+            <span>Title</span>
+            <span>?</span>
+          </div>
+          <div>
+            <span>Released</span>
+            <span>?</span>
+          </div>
+          <div>
+            <span>Season</span>
+            <span>?</span>
+          </div>
+          <div>
+            <span>Episode</span>
+            <span>?</span>
+          </div>
+          <div>
+            <span>Rating</span>
+            <span>
+              <StarIcon star-shine />
+              <StarIcon star-shine />
+              <StarIcon />
+              <StarIcon />
+              <StarIcon />
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
